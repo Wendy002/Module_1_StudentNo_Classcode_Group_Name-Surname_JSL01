53da3568527ff30f99b90828abf5af5479899b8f
Noxolo <wendyngcobo98@gmail.com>
2024-03-13 22:06:16 +0200

    Update spacing in js file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function validateSyntax() {
     // Validation logic goes here
     if (input[0,4] === "pet_" && _.isNumber(input[4,8])){
 
-        let result = `Valid Syntax \U+1F4AF	`
+        let result = `Valid Syntax \U+1F4AF`;
     }  else{
 
         let result = `Invalid Syntax \U+1FAE3`;
@@ -13,7 +13,7 @@ function validateSyntax() {
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+    document.getElementById('result').innerText = result;
 }
 
 
