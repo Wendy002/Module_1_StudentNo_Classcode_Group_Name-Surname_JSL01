5cf2f96254ffa30cc1bf99134f679ad8b7b79c88
Noxolo <wendyngcobo98@gmail.com>
2024-03-13 23:31:12 +0200

    Add comments

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ function validateSyntax() {
 
     if (input.slice(0,4) !== "pet_" && isNaN(input.slice(4,8))){
 
-        
+        //invalid syntax
         result = `Invalid Syntax \u{1FAE3} `;
         
     }  else{
-
+           //valid syntax
         result = `Valid Syntax \u{1F4AF}`;
         
     }
 
-
+    // output on the screen using id 'result'
     document.getElementById('result').textContent = result;
 }
 
