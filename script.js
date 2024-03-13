4a840a59508dc49207ebd4405fee6536fd24cb80
Noxolo <wendyngcobo98@gmail.com>
2024-03-13 21:33:42 +0200

    Add statements to display on browser

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     if (input[0,4] === "pet_" && _.isNumber(input[4,8])){
-        
+
+        let result = `Valid Syntax \U+1F4AF	`
+    }  else{
+
+        let result = `Invalid Syntax \U+1FAE3`;
     }
-    let result = ''; // Placeholder for validation result
+
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
