bb003c87dcdbaf63f16069bcfcc7ac787d9b4d57
Noxolo <wendyngcobo98@gmail.com>
2024-03-19 14:44:44 +0200

    Update

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function validateSyntax() {
  
-    const input = document.getElementById('petInput').value;
+    let input = document.getElementById('petInput').value;
     // Validation logic goes here
     // declare result here
-    let result;
+    let result= "";
+    const validPattern =/^pet_\d{4}[^\d_]+$/;
 
-    if (input.slice(0,4) !== "pet_" && isNaN(input.slice(4,8))){
-
-        //invalid syntax
-        result = `Invalid Syntax \u{1FAE3} `;
-        alert("Enter Valid input :(");
-        
-    }  else{
-           //valid syntax
+    if (validPattern.test(input)){
+        //valid syntax
         result = `Valid Syntax \u{1F4AF}`;
         alert("Yay Valid input!!!");
         
+    }  else{
+        //invalid syntax
+        result = `Invalid Syntax \u{1FAE3} `;
+        alert("Enter Valid input :(");
     }
 
     // output on the screen using id 'result'
